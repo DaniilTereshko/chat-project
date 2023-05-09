e81699f48d5545f5b07437f3eb55ee4b144f420d
Dzianis Tatur <tatdzi@tatdzi.local>
2023-05-09 15:02:25 +0300

    message v1.1

---
+++ src/test/java/by/chat/services/MessageServiceTest.java
@@ -0,0 +1,20 @@
+package by.chat.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MessageServiceTest {
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void testGet() {
+    }
+
+    @Test
+    void save() {
+    }
+}
