714329314d48b5ce28f3950aeb6c6f96103558a7
Daniil Tereshko <daniiltereshko123@gmail.com>
2023-05-09 10:10:27 +0300

    Structure.

--- src/main/java/by/chat/servlets/HomeServlet.java
+++ src/main/java/by/chat/servlets/HomeServlet.java
@@ -1,4 +1,22 @@
 package by.chat.servlets;
 
-public class HomeServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "HomeServlet", urlPatterns = "/")
+public class HomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("WEB-INF/home.jspx").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //req.getRequestDispatcher("WEB-INF/home.jspx").forward(req, resp);
+    }
 }
---
+++ src/main/webapp/WEB-INF/header.jsp
@@ -0,0 +1,9 @@
+
+<header>
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Messages</a></li>
+        </ul>
+    </nav>
+</header>
