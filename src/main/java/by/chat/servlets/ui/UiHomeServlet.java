bda413963820f155d44f3337cc9f05b1d13744ce
Dzianis Tatur <tatdzi@tatdzi.local>
2023-05-15 13:02:33 +0300

    add users on home page

--- src/main/java/by/chat/servlets/ui/UiHomeServlet.java
+++ src/main/java/by/chat/servlets/ui/UiHomeServlet.java
@@ -1,5 +1,7 @@
 package by.chat.servlets.ui;
 
+import by.chat.services.api.IUserService;
+import by.chat.services.factory.UserServiceFactory;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -10,8 +12,16 @@ import java.io.IOException;
 
 @WebServlet(name = "HomeServlet", urlPatterns = "/ui/")
 public class UiHomeServlet extends HttpServlet {
+    private IUserService userService;
+
+    public UiHomeServlet() {
+        this.userService = UserServiceFactory.getInstance();
+    }
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.setAttribute("users",userService.get());
         req.getRequestDispatcher("/ui/home.jspx").forward(req, resp);
     }
 }
