be8a26745ab7c7ba6705bb5abd32fa0dc69f823b
Dzianis Kuzko <dkuzko@tut.by>
2023-05-15 12:38:30 +0300

    Add statistic

--- src/main/java/by/chat/listeners/SessionListener.java
+++ src/main/java/by/chat/listeners/SessionListener.java
@@ -1,8 +1,8 @@
 package by.chat.listeners;
 
 import by.chat.core.dto.UserDTO;
-import by.chat.services.api.IStatisticsService;
-import by.chat.services.factory.StatisticsServiceFactory;
+import by.chat.services.api.IUserStatisticsService;
+import by.chat.services.factory.UserStatisticServiceFactory;
 import jakarta.servlet.annotation.WebListener;
 import jakarta.servlet.http.HttpSessionEvent;
 import jakarta.servlet.http.HttpSessionListener;
@@ -12,7 +12,7 @@ public class SessionListener implements HttpSessionListener {
 
     @Override
     public void sessionDestroyed(HttpSessionEvent se) {
-        IStatisticsService statisticsService = StatisticsServiceFactory.getInstance();
+        IUserStatisticsService statisticsService = UserStatisticServiceFactory.getInstance();
         // если сессия содержала объект user и была уничтожена по каким-либо причинам учитываем этот факт
         UserDTO user = (UserDTO) se.getSession().getAttribute("user");
         if (user != null) {
